7e532076d7f16501a906239f3197da3570ee3110
Toshiyuki Shimono <bin4tsv@gmail.com>
2021-05-28 16:37:32 +0900

    最初に最低限必要と思われる4個のファイルを配置して手で編集

---
+++ Bin4TSV.pm
@@ -0,0 +1,44 @@
+package Bundle::Bin4TSV ; 
+use strict ; 
+use warnings ; 
+
+$VERSION = "0.330" ; 
+$DATE = "2021-05-28T16:30+0900" ; 
+
+1 ; 
+
+__END__
+
+=head1 NAME
+
+Bundle::Bin4TSV - Bundle related to "Bin4TSV"
+
+=head1 SYNOPSIS
+
+ perl -MCPAN -e 'install Bundle::Bin4TSV'
+  or
+ cpanm Bundle::Bin4TSV
+
+=head1 DESCRIPTION
+
+=head1 CONTENTS
+
+Bin::Data::1D
+
+Bin::File::Dir
+
+Bin::File::Time
+
+Bin::Gen::Rand
+
+Bin::Subtotal
+
+Bin::TSV::Conv
+
+Bin::Text::Color::Plus
+
+=head1 AUTHOR
+
+Toshiyuki SHIMONO <bin4tsv at gmail.com>
+
+=cut
